f897f1980454438be6eeb069a0e27007bc39d5c9
sidulova <sidul001@gwu.edu>
2022-06-27 20:18:53 -0400

    mnist task v1

--- main_out.py
+++ main_out.py
@@ -3,7 +3,18 @@ from libdg.compos.exp.exp_cuda_seed import set_seed  # reproducibility
 from libdg.arg_parser import parse_cmd_args
 
 if __name__ == "__main__":
+    import torch
+    import sys
+    print('torch version', torch.__version__,torch.cuda.is_available(), torch.cuda.device_count(), torch.version.cuda)
+    print('python version', sys.version)
     args = parse_cmd_args()
+    print(args)
+    breakpoint()
+    if args.task == 'mnist':
+        from domid.compos.exp.exp_main import Exp
+
     set_seed(args.seed)
+    #breakpoint()
     exp = Exp(args=args)
+
     exp.execute()
