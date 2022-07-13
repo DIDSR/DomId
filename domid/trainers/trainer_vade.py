82233fb7f1c16ae562788bf771929e71411b434c
sidulova <sidul001@gwu.edu>
2022-07-13 16:33:42 -0400

    seperated pretraining in a different class

--- domid/trainers/trainer_vade.py
+++ domid/trainers/trainer_vade.py
@@ -8,6 +8,7 @@ from libdg.algos.trainers.a_trainer import TrainerClassif
 import torch.optim as optim
 from tensorboardX import SummaryWriter
 from sklearn.manifold import TSNE
+from domid.trainers.pretraining import pretraining
 
 
 
@@ -23,30 +24,37 @@ class TrainerVADE(TrainerClassif):
         self.epo_loss_tr = 0
         #breakpoint()
         counter = 0
+        mse_n = 2
         # if epoch<10 (make a differnet file)
         #MSE loss - loss = self.MSE_cal_loss
-
+        p = pretraining(self.model, self.device, self.optimizer, self.epo_loss_tr, self.loader_tr)
         for _, (tensor_x, vec_y, vec_d) in enumerate(self.loader_tr):
             tensor_x, vec_y, vec_d = \
                 tensor_x.to(self.device), vec_y.to(self.device), vec_d.to(self.device)
             self.optimizer.zero_grad()
-            loss = self.model.cal_loss(tensor_x)
+            if epoch < mse_n:
+                loss = p.pretrain_loss(tensor_x, mse_n, epoch)
+            else:
+                loss = self.model.cal_loss(tensor_x)
 
             loss = loss.sum()
-
-            if epoch == 10:
-                #self.writer.add_embedding(tsne, metadata = meta, label_img=tensor_x)
-                #self.writer.add_embedding(X, label_img=tensor_x)
-                prediction, z_mu, z, log_sigma2_c, yita, x_pro = self.model.infer_d_v_2(tensor_x)
-                #X = torch.flatten(z_mu, start_dim=1).cpu()
-                # num = torch.argmax(vec_y, 1).cpu()
-                # color = torch.argmax(vec_d, 1).cpu()
-
-                self.writer.add_embedding(z_mu, label_img=x_pro)
-
             loss.backward()
             self.optimizer.step()
             self.epo_loss_tr += loss.detach().item()
+        if epoch < mse_n:
+            p.GMM_fit()
+
+            # if epoch == 10:
+            #     #self.writer.add_embedding(tsne, metadata = meta, label_img=tensor_x)
+            #     #self.writer.add_embedding(X, label_img=tensor_x)
+            #     prediction, z_mu, z, log_sigma2_c, yita, x_pro = self.model.infer_d_v_2(tensor_x)
+            #     #X = torch.flatten(z_mu, start_dim=1).cpu()
+            #     # num = torch.argmax(vec_y, 1).cpu()
+            #     # color = torch.argmax(vec_d, 1).cpu()
+            #
+            #     self.writer.add_embedding(z_mu, label_img=x_pro)
+            #
+
 
 
             #print('Shapes for epcoh', counter, epoch, pred.shape, pi.shape, mu.shape, sigma.shape, yita.shape, x_pro.shape)
@@ -71,8 +79,10 @@ class TrainerVADE(TrainerClassif):
             name = "Input to the encoder" + str(epoch)
             self.writer.add_images(name, tensor_x, 0)
 
-        name = "Output of the decoder"+str(epoch)
-        self.writer.add_images(name, x_pro, 0)
+        name = "Input vs Output of the decoder"+str(epoch)
+        imgs = torch.cat((tensor_x[0:8, :, :, :], x_pro[0:8, :, :, :],), 0)
+        self.writer.add_images(name, imgs, 0)
+        #self.writer.add_images(name, tensor_x[0:8, :, :, :], 0)
             # self.writer.add_image('Input epoch = 10 ', x_pro[2, :, :, :], 0)
             # self.writer.add_image('Input epoch = 10 ', x_pro[3, :, :, :], 0)
 
--- domid/trainers/trainer_vade_pretraining.py
+++ domid/trainers/trainer_vade_pretraining.py
@@ -6,111 +6,85 @@ import torch.optim as optim
 from libdg.algos.trainers.a_trainer import TrainerClassif
 
 from domid.utils.perf_cluster import PerfCluster
+from domid.trainers.pretraining import pretraining
 
 
 class TrainerVADE(TrainerClassif):
     def __init__(self, model, task, observer, device, writer, aconf=None):
         super().__init__(model, task, observer, device, aconf)
+
+        self.LR = aconf.lr
         self.optimizer = optim.Adam(self.model.parameters(), lr=aconf.lr)
         self.epo_loss_tr = None
         self.writer = writer
 
+        #self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=2, gamma=0.1)
+        # step_size = 10, gamma = 0.95
+        #optimizer Adam
+
+    def plot_loss_epoch(self, mse_loss, elbo_loss):
+        x_mse = np.arrange(mse_n)
+        x_elbo = np.arrange(elbo_loss)
+        import matplotlib.pyplot as plt
+        plt.figure()
+        plt.subplot(2,1,1)
+        plt.plot(x_mse, mse_loss)
+        plt.title('MSE loss')
+
+        plt.subplot(2,1,2)
+        plt.plot(x_elbo, elbo_loss)
+        plt.title('ELBO loss')
+        return plt.show()
+
+
     def tr_epoch(self, epoch):
+        print('LEARNING RATE', self.LR)
         self.model.train()
         self.epo_loss_tr = 0
         #breakpoint()
-        mse_n = 10  # FIXME: maybe have a command line argument to specify mse_n and elbo_n
-        elbo_n = 30
-        #breakpoint()
+        mse_n =5 # FIXME: maybe have a command line argument to specify mse_n and elbo_n
+        elbo_n = 300
+
+        # if epoch>mse_n-2:
+        #     self.LR = 0.0000001
+        p = pretraining(self.model, self.device, self.optimizer, self.epo_loss_tr, self.loader_tr)
         for i, (tensor_x, vec_y, vec_d) in enumerate(self.loader_tr):
-            tensor_x = tensor_x.to(self.device)
-            self.optimizer.zero_grad()
-            if epoch < mse_n:
-                loss = self.model.pretrain_loss(tensor_x)
-                #print("LOOOOSSS", loss)
+            tensor_x, vec_y, vec_d = \
+                tensor_x.to(self.device), vec_y.to(self.device), vec_d.to(self.device)
+            if epoch<mse_n:
+                loss = p.pretrain_loss(tensor_x, mse_n,epoch)
             else:
                 loss = self.model.cal_loss(tensor_x)
 
             loss = loss.sum()
-            #print("LOSS back", loss)
+            # print("LOSS back", loss)
             loss.backward()
             self.optimizer.step()
+            # self.scheduler.step()
             self.epo_loss_tr += loss.cpu().detach().item()
 
-        # During pre-training we estimate pi, mu_c, and log_sigma2_c with a GMM at the end of each epoch.
-        # After pre-training these initial parameter values are used in the calculation of the ELBO loss,
-        # and are further updated with backpropagation like all other neural network weights.
-        if epoch < mse_n:
-            num_img = len(self.loader_tr.dataset)
-            Z = np.zeros((num_img, self.model.zd_dim))
-            counter = 0
-            for tensor_x, vec_y, vec_d in self.loader_tr:
-                tensor_x = tensor_x.to(self.device)
-                preds, z_mu, z, _, _, x_pro = self.model.infer_d_v_2(tensor_x)
-                z = z.detach().cpu().numpy() #[batch_size, zd_dim]
-                Z[counter:counter+z.shape[0], :] = z
-                counter += z.shape[0]
-
-            #breakpoint()
-            gmm = GaussianMixture(n_components=self.model.d_dim, covariance_type='diag', reg_covar=10 ** -4)
-            # breakpoint()
-            pre = gmm.fit_predict(Z)
-            # gmm = gmm.fit(Z) #FIXME
-            # print(gmm.weights_[1:3], '\n', gmm.means_[0, 1:3])
-
-            self.model.pi_.data = torch.from_numpy(gmm.weights_).to(self.device).float()
-            self.model.mu_c.data = torch.from_numpy(gmm.means_).to(self.device).float()
-            self.model.log_sigma2_c.data = torch.log(torch.from_numpy(gmm.covariances_)).to(self.device).float()
+        if epoch<mse_n:
+            p.GMM_fit()
 
 
+        preds, z_mu, z, _, _, x_pro = self.model.infer_d_v_2(tensor_x)
+        name = "Output of the decoder" + str(epoch)
+        imgs = torch.cat((tensor_x[0:8,:, :, :], x_pro[0:8,:, :, :],), 0)
+        self.writer.add_images(name, imgs, 0)
+        self.writer.add_scalar('Learning rate', self.LR, epoch)
+
         #
-        # from sklearn.metrics.pairwise import cosine_similarity
-        # from scipy import sparse
-        # from sklearn import metrics
-        # #acc_tr_pool, conf_mat_tr = PerfCluster.cal_acc(self.model, self.loader_tr, self.device)
-        # #acc_val, conf_mat_val = PerfCluster.cal_acc(self.model, self.loader_te, self.device)
-        # preds, z_mu, z, _, _, x_pro = self.model.infer_d_v_2(tensor_x)
-        # z = z.cpu()
-        # #self.writer.add_scalar("pooled train clustering acc: ", acc_tr_pool, epoch)
-        # #self.writer.add_scalar("clustering validation acc: ", acc_val)
-        # # import numpy as np
-        # # #breakpoint()
-        # # flat_a, flat_b = torch.flatten(tensor_x, 1), torch.flatten(x_pro,1)
-        # # flat_a, flat_b = torch.flatten(flat_a, 1), torch.flatten(flat_b, 1)
-        # # flat_a, flat_b = torch.flatten(flat_a, 1), torch.flatten(flat_b, 1)
-        # #
-        # # breakpoint()
-        # #
-        # # #a_sparse, b_sparse = sparse.csr_matrix(flat_a), sparse.csr_matrix(flat_b)
-        # # flat_a = flat_a.detach().numpy()
-        # # flat_b = flat_b.detach().numpy()
-        # #
-        # # sim_sparse = cosine_similarity(flat_a, flat_b, dense_output=False)
-        # #
-        # #
-        # # from torchmetrics import Accuracy, F1Score
-        # # acc = Accuracy()
-        # # F1 = F1Score(num_classes=7)
-        # # reconstruction_acc = np.mean(sim_sparse)
-        # # clustering_acc = acc(torch.argmax(preds, 1), torch.argmax(vec_d, 1))
-        #
-        #
-        #
-        #
-        # name = "Output of the decoder" + str(epoch)
-        # imgs = torch.cat((tensor_x[0:8,:, :, :], x_pro[0:8,:, :, :],), 0)
-        # self.writer.add_images(name, imgs, 0)
-        #
-        # if epoch<mse_n:
-        #     self.writer.add_scalar('MSE loss', self.epo_loss_tr, epoch)
-        # else:
-        #     self.writer.add_scalar('ELBO loss', self.epo_loss_tr, epoch)
-        #     #self.writer.add_scalar('Reconstraction Accuracy (cos similarity)', reconstruction_acc, epoch)
-        #     #self.writer.add_scalar('Domain clustering acc', clustering_acc, epoch)
-        # if epoch ==elbo_n:
-        #
-        #     class_labels = torch.argmax(vec_y, 1)
-        #     self.writer.add_embedding(z, metadata= class_labels, label_img=x_pro) #FIXME set global trainer step
+        if epoch<mse_n:
+            self.writer.add_scalar('MSE loss', self.epo_loss_tr, epoch)
+        else:
+            self.writer.add_scalar('ELBO loss', self.epo_loss_tr, epoch)
+            #self.writer.add_scalar('Reconstraction Accuracy (cos similarity)', reconstruction_acc, epoch)
+            #self.writer.add_scalar('Domain clustering acc', clustering_acc, epoch)
+        if epoch ==elbo_n:
+
+            class_labels = torch.argmax(vec_y, 1)
+
+            self.writer.add_embedding(z, metadata= class_labels, label_img=x_pro) #FIXME set global trainer step
 
         flag_stop = self.observer.update(epoch)  # notify observer
 
