cbf274fca5aaef2e316edea20e668e6006a9a7ed
agisga <11449372+agisga@users.noreply.github.com>
2022-09-20 22:46:42 -0400

    docs and minor fixer for the HER2 and MNIST tasks

--- domid/tasks/task_her2.py
+++ domid/tasks/task_her2.py
@@ -1,69 +1,68 @@
-"""
-HER2 task where the HER2 categories are considered "domains"
-"""
-
 from torch.utils.data import random_split
 from torchvision import transforms
 from domainlab.tasks.utils_task import ImSize
 from domainlab.utils.utils_classif import mk_dummy_label_list_str
-#from domainlab.tasks.b_task import NodeTaskDict
 from domainlab.tasks.b_task import NodeTaskDict
 from domid.dsets.dset_her2 import DsetHER2
 
 
 class NodeTaskHER2(NodeTaskDict):
     """
-    Based on NodeTaskMNISTColor10 from DomainLab.
-    The digits (0, 1, ..., 9) are regarded as domains (to be separated by unsupervised clustering).
+    HER2 task where the HER2 categories are considered "domains"
     """
 
-    # def init_business(self, a):
-    #     print('i do not know what this function is')
     @property
     def list_str_y(self):
         """
-        MNIST task has no labels (digits are considered domains)
+        Labels are not used in clustering. So, we just return a dummy list for now (for compatibility with domainlab).
         """
-        #['FD', 'ND', 'H1', 'H2']
         return mk_dummy_label_list_str("dummy", 3)
 
     @property
     def isize(self):
-        """image channel, height, width"""
+        """
+        :return: image size object storing image channels, height, width.
+        """
         return ImSize(3, 100, 100)  # FIXME should be in sync with transforms
 
     def get_list_domains(self):
         """
         Get list of domain names
+        :return: list of domain names
         """
         return mk_dummy_label_list_str("class", 3)
 
-    def get_dset_by_domain(self, args, na_domain, split=True):  # , na_domain, split=True):
-        """get_dset_by_domain.
-        :param args:
-        :param na_domain:
-        :param split: for test set, no need to split
-        args.split: by default, split is set to be zero which in python can
-        be evaluated in if statement, in which case, no validation set will be
-        created. Otherwise, this argument is the split ratio
+    def get_dset_by_domain(self, args, na_domain, split=True):
+        """Get a dataset by domain name
+        :param args: command line arguments
+        :param na_domain: domain name
+        :param split: whether a training/validation split is performed (the
+        training split portion will be determined by args.split); for test
+        set, no need to split; args.split: by default, split is set to be
+        zero which in python can be evaluated in if statement, in which case,
+        no separate validation set will be created. Otherwise, this argument
+        is the percentage of the data to be used as training set, while the 
+        rest will be used as validation set.
+        :return: training dataset, validation dataset
         """
-        split = True
         ratio_split = float(args.split) if split else False
         # by default, split is set to be zero which in python can
         # be evaluated in if statement, in which case, no validation
         # set will be created. Otherwise, this argument is
         # the split ratio
         ind_global = self.get_list_domains().index(na_domain)
+        # TODO: add a comment about where these numbers come from
         mean = [0.6399, 0.5951, 0.6179]
         std = [0.1800, 0.1980, 0.2070] #[0.1582, 0.1728, 0.1728]
         #mean = [0.4, 0.4, 0.4]
         #mean = [0.5, 0.5, 0.5]
-        #confirm mean 0 and std 1 after the normalization
-        trans = transforms.Compose([transforms.Resize((100, 100)), transforms.RandomHorizontalFlip(),
+        # TODO: confirm mean 0 and std 1 after the normalization
+        trans = transforms.Compose([transforms.Resize((100, 100)),
+                                    transforms.RandomHorizontalFlip(),
                                     transforms.RandomVerticalFlip(),
                                     transforms.ToTensor()]) #, transforms.Normalize(mean, std)]) transforms.RandomRotation(45),
+        # TODO: do or don't do normalization?
         dset = DsetHER2(ind_global, args.dpath, transform=trans)
-
         train_set = dset
         val_set = dset
         # split dset into training and validation sets
