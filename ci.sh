c6f22df49ecf5bd365fa7055cf1d2fcad74e792f
agisga <11449372+agisga@users.noreply.github.com>
2022-12-01 12:26:12 -0500

    sets up CI to run automated tests through github actions

---
+++ ci.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x -v
+set -e  # exit upon first error
+starttime=`date +%s`
+#bash -x -v ci_run_examples.sh
+sh ci_pytest_cov.sh
+#sh gen_doc.sh
+endtime=`date +%s`
+runtime=$((endtime-starttime))
+echo "total time used:"
+echo "$runtime"
+
