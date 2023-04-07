fcb078cb3f97493763c3bc205a8eb79fbcc84476
agisga <11449372+agisga@users.noreply.github.com>
2023-04-07 12:27:53 -0400

    improves dataframe generation script for HER2 dataset

--- domid/dsets/generate_dataset_dataframe_her2.py
+++ domid/dsets/generate_dataset_dataframe_her2.py
@@ -1,49 +1,78 @@
 import os
 import pandas as pd
 import numpy as np
-path = '../../DomId/HER2/combined_train'
-folders = os.listdir(path)
+import sys
+
+try:
+    path = sys.argv[1]
+except IndexError:
+    path = "../../DomId/HER2/combined_train"
+print(f"HER2 data directory: {path}")
+
+
+def get_jpg_folders(path):
+    """
+    only keep folders of .jpg images,
+    which folder names by convention end in jpg
+    """
+    folders = os.listdir(path)
+    jpg_folders = list(
+        filter(
+            lambda f: f.endswith("jpg") and os.path.isdir(os.path.join(path, f)),
+            folders,
+        )
+    )
+    return jpg_folders
+
 
 def total_count_images(path):
-    folders = os.listdir(path)
-    counter=0
+    folders = get_jpg_folders(path)
+    counter = 0
     for folder in folders:
         folder_path = os.path.join(path, folder)
         images = os.listdir(folder_path)
-        counter+=len(images)
-        print(len(images), counter)
+        counter += len(images)
+        print(folder, len(images), counter)
     return counter
+
+
 def parse_machine_labels(image_names):
-    machine_labels =[]
-    machine_dict = {'FD': 0, 'H1': 1, 'H2': 2, 'ND': 3}
+    machine_labels = []
+    machine_dict = {"FD": 0, "H1": 1, "H2": 2, "ND": 3}
     for image in image_names:
         machine = image[-6:-4]
         machine = machine_dict[machine]
         machine_labels.append(machine)
     return machine_labels
 
-number_labels = 2
-N = total_count_images(path)
-data =np.zeros((N, number_labels+1)).astype('str')
-start = 0
 
+if __name__ == "__main__":
 
-for folder in folders:
-    label_of_the_folder_int = int(folder[-4])
-    print(folder)
-    print('start', start)
-    folder_path = os.path.join(path, folder)
-    images = os.listdir(folder_path)
-    labels = [label_of_the_folder_int]*len(images)
-    machine_labels = parse_machine_labels(images)
+    folders = get_jpg_folders(path)
+    N = total_count_images(path)
+    number_labels = 2
+    data = np.zeros((N, number_labels + 1)).astype("str")
+    start = 0
 
-    data[start:start+len(images), :] = np.stack((images, labels, machine_labels),0).T
-    start+=len(images)
-    print(len(images))
+    for folder in folders:
+        print(folder)
+        print("start", start)
+        label_of_the_folder_int = int(folder[-4])
+        folder_path = os.path.join(path, folder)
+        images = os.listdir(folder_path)
+        labels = [label_of_the_folder_int] * len(images)
+        machine_labels = parse_machine_labels(images)
 
+        data[start : start + len(images), :] = np.stack(
+            (images, labels, machine_labels), 0
+        ).T
+        start += len(images)
+        print(len(images))
 
-
-
-
-dataframe =pd.DataFrame(data)
-dataframe.to_csv('../HER2/combined_train/dataframe.csv', header = ['img_id', 'class', 'machine'],index=False)
+    dataframe = pd.DataFrame(data)
+    csv_path = os.path.join(path, "dataframe.csv")
+    dataframe.to_csv(
+        csv_path,
+        header=["img_id", "class", "machine"],
+        index=False,
+    )
