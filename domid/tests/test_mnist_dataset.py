47001766828cc591369106b253abdb1c4a67dbf7
sidulova <sidul001@gwu.edu>
2023-04-11 14:03:37 -0400

    test for checking the length of the datasets

---
+++ domid/tests/test_mnist_dataset.py
@@ -0,0 +1,79 @@
+from domid.arg_parser import mk_parser_main
+from domid.compos.exp.exp_main import Exp
+import torch
+from domid.models.model_m2yd import ModelXY2D
+from domid.models.model_vade import ModelVaDE
+from domid.tasks.task_mnist import NodeTaskMNIST
+from domid.tasks.task_mnist_color import NodeTaskMNISTColor10
+
+def node_compiler(args):
+    if args.task == "mnist":
+        node = NodeTaskMNIST()
+        domain = "digit2"
+    elif args.task == "mnistcolor10":
+        node = NodeTaskMNISTColor10()
+        domain = "rgb_31_119_180"
+
+    dset2 = node.get_dset_by_domain(args, domain)
+    ldr = torch.utils.data.DataLoader(dset2[0]) #train set from the task
+    return ldr
+def test_mnist_length():
+    parser = mk_parser_main()
+    args = parser.parse_args(
+        [
+            "--te_d",
+            "7",
+            "--tr_d",
+            "1"
+            "--zd_dim",
+            "5",
+            "--d_dim",
+            "1",
+            "--dpath",
+            "zout",
+            "--split",
+            "0.8",
+            "--L",
+            "5",
+            "--prior",
+            "Bern",
+            "--model",
+            "linear",
+            "--task",
+            "mnist"
+
+        ]
+    )
+    ldr = node_compiler(args)
+    print(len(ldr))
+    assert len(ldr) > 4000
+def test_mnistcolor10_length():
+    parser = mk_parser_main()
+    args = parser.parse_args(
+        [
+            "--te_d",
+            "7",
+            "--tr_d",
+            "1",
+            "--zd_dim",
+            "5",
+            "--d_dim",
+            "1",
+            "--dpath",
+            "zout",
+            "--split",
+            "0.8",
+            "--L",
+            "5",
+            "--prior",
+            "Bern",
+            "--model",
+            "linear",
+            "--task",
+            "mnistcolor10"
+
+        ]
+    )
+
+    ldr = node_compiler(args)
+    assert len(ldr) <1000
