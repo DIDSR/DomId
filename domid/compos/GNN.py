84db023c6df3e2fe9386b43c137c8a9b8444d852
sidulova <sidul001@gwu.edu>
2023-05-12 10:58:36 -0400

    gnn layers and model

---
+++ domid/compos/GNN.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+from torch.nn.modules.module import Module
+from domid.compose.GNN_layer import GNNLayer
+
+class GNN(Module):
+    def __int__(self, n_input, n_enc_1, n_enc_2, n_enc_3, n_z, n_clusters):
+        super(GNN, self).__init__()
+        self.gnn_1 = GNNLayer(n_input, n_enc_1)
+        self.gnn_2 = GNNLayer(n_enc_1, n_enc_2)
+        self.gnn_3 = GNNLayer(n_enc_2, n_enc_3)
+        self.gnn_4 = GNNLayer(n_enc_3, n_z)
+        self.gnn_5 = GNNLayer(n_z, n_clusters)
+    def foward(self, x, adj, tra1, tra2, tra3, z):
+        h = self.gnn_1(x, adj)
+        sigma = 0.5
+        h = self.gnn_2((1 - sigma) * h + sigma * tra1, adj)
+        h = self.gnn_3((1 - sigma) * h + sigma * tra2, adj)
+        h = self.gnn_4((1 - sigma) * h + sigma * tra3, adj)
+        h = self.gnn_5((1 - sigma) * h + sigma * z, adj, active=False)
+
+        return h
---
+++ domid/compos/linear_AE.py
@@ -0,0 +1,34 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+from domid.compos.VAE_blocks import linear_block
+import torch.nn.functional as F
+
+
+class LinearAE(nn.Module):
+    def __init__(self, n_enc_1, n_enc_2, n_enc_3, n_dec_1, n_dec_2, n_dec_3,
+                 n_input, n_z):
+        super(LinearAE, self).__init__()
+        self.enc_1 = nn.Linear(n_input, n_enc_1)
+        self.enc_2 = nn.Linear(n_enc_1, n_enc_2)
+        self.enc_3 = nn.Linear(n_enc_2, n_enc_3)
+        self.z_layer = nn.Linear(n_enc_3, n_z)
+
+        self.dec_1 = nn.Linear(n_z, n_dec_1)
+        self.dec_2 = nn.Linear(n_dec_1, n_dec_2)
+        self.dec_3 = nn.Linear(n_dec_2, n_dec_3)
+        self.x_bar_layer = nn.Linear(n_dec_3, n_input)
+
+    def forward(self, x):
+        enc_h1 = F.relu(self.enc_1(x))
+        enc_h2 = F.relu(self.enc_2(enc_h1))
+        enc_h3 = F.relu(self.enc_3(enc_h2))
+        z = self.z_layer(enc_h3)
+
+        dec_h1 = F.relu(self.dec_1(z))
+        dec_h2 = F.relu(self.dec_2(dec_h1))
+        dec_h3 = F.relu(self.dec_3(dec_h2))
+        x_bar = self.x_bar_layer(dec_h3)
+
+        return x_bar, enc_h1, enc_h2, enc_h3, z
