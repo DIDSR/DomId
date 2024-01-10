ed3d9fc24540593ef54de0288cf9cdec159160fe
sidulova <sidul001@gwu.edu>
2024-01-10 15:28:46 -0500

    a model wipm

--- domid/models/a_model_cluster.py
+++ domid/models/a_model_cluster.py
@@ -9,6 +9,8 @@ class AModelCluster(nn.Module):
     """
     Operations that all clustering models should have
     """
+    def __init__(self):
+        self._decoratee = None
 
     def create_perf_obj(self, task):
         """
@@ -39,9 +41,59 @@ class AModelCluster(nn.Module):
                 r_score_te = self.perf_metric_correlation.cal_acc(self, loader_te, device)
 
         return metric_tr, metric_te, r_score_tr, r_score_te
+
+    def extend(self, model):
+        """
+        extend the loss of the decoratee
+        """
+        self._decoratee = model
+        self.reset_feature_extractor(model.net_invar_feat)
     def calc_loss(self, tensor_x, vec_y, vec_d, inj_tensor, img_ids):
         """
         Calculates the loss for the model.
         """
+
         raise NotImplementedError
 
+    def infer_d_v(self, x):
+        """
+        Predict the cluster/domain of the input data.
+        Corresponds to equation (16) in the paper.
+
+        :param tensor x: Input tensor of a shape [batchsize, 3, horzintal dim, vertical dim].
+        :return tensor preds: One hot encoded tensor of the predicted cluster assignment.
+        """
+        preds, *_ = self._inference(x)
+        return preds.cpu().detach()
+
+    def _extend_loss(self, tensor_x, tensor_y, tensor_d, others=None):
+        """
+        combine losses from two models
+        """
+        if self._decoratee is not None:
+            return self._decoratee.cal_rec_loss(
+                tensor_x, tensor_y, tensor_d, others)
+        return None, None
+    def _cal_pretrain_loss(self, tensor_x, x_pro, inject_domain):
+        """
+        Pretraining loss for the model.
+        """
+        Loss = nn.MSELoss()
+        pre_loss = Loss(tensor_x, x_pro)
+        raise pre_loss
+    def _cal_reconstruction_loss(self, x, x_pro):
+        """
+        Reconstruction loss for the model.
+        """
+        sigma = torch.Tensor([0.9]).to(self.device)  # mean sigma of all images
+        log_sigma_est = torch.log(sigma).to(self.device)
+        rec_loss = torch.mean(torch.sum(torch.sum(torch.sum(0.5 * (x - x_pro) ** 2, 2), 2), 1), 0) / sigma ** 2
+
+        raise rec_loss
+    def _cal_kl_loss(self, q, p):
+        """
+
+        :return:
+        """
+        kl_loss = F.kl_div(q.log(), p, reduction="batchmean")
+        return kl_loss
