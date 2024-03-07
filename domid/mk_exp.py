ca0ed54834735603eba273af8f7dc578628b40a9
sidulova <sidul001@gwu.edu>
2024-03-07 15:56:49 -0500

    updated mk_exp

--- domid/mk_exp.py
+++ domid/mk_exp.py
@@ -5,10 +5,10 @@ from domid.arg_parser import mk_parser_main
 from domid.compos.exp.exp_main import Exp
 
 
-def mk_exp(task, model, trainer: str, test_domain: str, batchsize: int, nocu=True, prior = 'Bern',
+def mk_exp(task, model, trainer: str,train_domain:str, test_domain:str, batchsize: int, nocu=True, prior = 'Bern',
             random_batching=False,
             model_method='cnn',
-            feat_extract='vae'):
+            feat_extract='vae', pre_tr =25):
     """
     Creates a custom experiment. The user can specify the input parameters.
 
@@ -25,17 +25,21 @@ def mk_exp(task, model, trainer: str, test_domain: str, batchsize: int, nocu=Tru
 
     Returns: experiment
     """
+    print(random_batching)
     str_arg = (
-        f"--model={model} --trainer={trainer} --te_d={test_domain} --bs={batchsize} --task={task} "
-        f"--prior={prior} --model_method={model_method} --feat_extract={feat_extract} --random_batching={random_batching}"
+        f"--model={model} --trainer={trainer} --bs={batchsize} --task={task} "
+        f"--prior={prior} --model_method={model_method} --feat_extract={feat_extract} --random_batching={random_batching} "
+        f"--pre_tr={pre_tr} --d_dim={len(train_domain.split(' '))}"
     )
     if nocu:
         str_arg += " --nocu"
+    str_arg+= " --tr_d "+train_domain
+    str_arg+= " --te_d "+test_domain
     parser = mk_parser_main()
-
     conf = parser.parse_args(str_arg.split())
+    print(conf)
     exp = Exp(conf, task, model=model)
     return exp
 
 if __name__ == "__main__":
-    mk_exp('mnistcolor10', 'vade', 'cluster', '0', 2)
+    mk_exp('mnistcolor10', 'vade', 'cluster', '0', '2', 2)
